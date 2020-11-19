5cbba8b87f341a9cc09efb7ab59a4a78b54fdd88
Saiph0 <72117903+Saiph0@users.noreply.github.com>
2020-11-19 06:17:45 -0800

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,11 @@
+// Name: Cynthia Luong
+// Date: 11/19/2020
+// Class: CS 3560.01
+//
+// Driver Class - triggers Admin Control Panel
+
+public class Driver {    
+   public static void main(String[] args) {
+      AdminControlPanel.getInstance().setVisible(true);
+   }
+}
